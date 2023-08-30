9d41c2a5877747beecfee281f198b150f7a56de9
AhmadAlzo <124163736+AhmadAlzo@users.noreply.github.com>
2023-08-30 08:48:41 +0300

    Update index.js

--- constans/index.js
+++ constans/index.js
@@ -3,4 +3,4 @@ import { getStatusBarHeight } from 'react-native-status-bar-height';
 export const SCREEN_HEIGHT = Math.round(Dimensions.get('window').height);
 export const SCREEN_WIDTH = Math.round(Dimensions.get('window').width);
 export const STATUSBAR_HEIGHT = getStatusBarHeight()
-export const BASE_URL = 'http://192.168.1.3:3000'
+export const BASE_URL = 'http://192.168.1.3:3000'  
